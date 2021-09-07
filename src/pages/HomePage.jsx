3eccafedd5cf8ce0dd81ff9862514407c9d4423c
Johan Nordström <johan@digitalvillage.se>
2021-09-07 09:51:04 +0200

    Added a nice welcome-message.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -5,6 +5,8 @@ const HomePage = () => {
 	return (
 		<Container className="py-3">
 			<h1>Welcome!</h1>
+
+			<p>This is a demo deploy of React Query to Netlify <span role="img" aria-label="a red heart">❤️</span></p>
 		</Container>
 	)
 }
